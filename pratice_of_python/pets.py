01b659f69502799424964463170ee98013bf4850
minyun168 <minyun168@outlook.com>
2019-01-05 00:24:17 +0800

    Create pets.py

---
+++ pratice_of_python/pets.py
@@ -0,0 +1,9 @@
+#pets.py
+
+def describe_pet(animal_type, pet_name):
+	"""The information of pets"""
+	print ("\nI have a "+animal_type+".")
+	print ("My "+animal_type+" is "+pet_name+".")
+
+describe_pet("pig","dengzhizhu")
+describe_pet("monkey","hou")
