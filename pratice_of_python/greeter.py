893246bd7446a6af24296971af9af58afbefac9b
minyun168 <minyun168@outlook.com>
2019-01-05 00:05:16 +0800

    Update greeter.py

--- pratice_of_python/greeter.py
+++ pratice_of_python/greeter.py
@@ -1,9 +1,9 @@
 # pratice of "Python Crash Course"  chapter 8 function
 
-def great_user():
+def great_user(username):
 	""" greating"""
-	print ("hello!girl")
+	print ("Hello, "+username.title()+"!")
 
-great_user()
+great_user("girl")
 
 
