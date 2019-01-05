c06e044565aa1de671d7a13d86aaa2647ef53a11
minyun168 <minyun168@outlook.com>
2019-01-06 00:01:37 +0800

    Update pets.py

--- pratice_of_python/pets.py
+++ pratice_of_python/pets.py
@@ -1,9 +1,10 @@
 #pets.py
 
-def describe_pet(animal_type, pet_name):
+def describe_pet(animal_type="pig", pet_name="dengzhizhu"):
 	"""The information of pets"""
 	print ("\nI have a "+animal_type+".")
 	print ("My "+animal_type+" is "+pet_name+".")
 
 describe_pet("pig","dengzhizhu")
 describe_pet("monkey","hou")
+describe_pet(animal_type="monkey", pet_name="hou")
