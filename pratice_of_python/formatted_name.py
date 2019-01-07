795cb4460e21f61cc29d5e455f9b1f3a1f968d67
minyun168 <minyun168@outlook.com>
2019-01-08 02:06:18 +0800

    Update formatted_name.py

--- pratice_of_python/formatted_name.py
+++ pratice_of_python/formatted_name.py
@@ -1,10 +1,17 @@
 
 
-def formatted_name(first_name, middle_name, last_name):
-	"""return formatted name"""
+def formatted_name(first_name, last_name, middle_name = ""):   #when we use default value just like middle_name="", we should make sure middle_name is the last variable when we define function
+	"""return formatted name"""								   # middle_name = "" not middle = " "
 
-	full_name = first_name + " " + middle_name + " " + last_name
+	if middle_name:
+		full_name = first_name + " " + middle_name + " " + last_name
+		
+	else:
+		full_name = first_name + " " + last_name
 	return full_name.title()
-
+	
 musician = formatted_name("sheeli", "arki", "habarum")
 print (musician)
+
+artist = formatted_name("shor", "ka")
+print (artist)
