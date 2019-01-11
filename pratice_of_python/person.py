4df15fd1d871cd5caed5adf9cfa7f535aea22b2d
minyun168 <minyun168@outlook.com>
2019-01-11 18:39:10 +0800

    Update person.py

--- pratice_of_python/person.py
+++ pratice_of_python/person.py
@@ -1,9 +1,13 @@
 # person.py
 
-def build_person(first_name, last_name):
+def build_person(first_name, last_name, age=''):
 	"""return a dictionary include the information of a person """
 	person = {'first':first_name, 'last':last_name}
-	return person
+	
+	if age:
+		person['age'] = age
+	#	print type(person['age'])  # here I want to check the type of age
+	return person 
 
-musician = build_person('jimi', 'hendrix')
+musician = build_person('jimi', 'hendrix', age = 27)   # here i can not writed as   'age'=27  //<crash python>P124
 print (musician)
