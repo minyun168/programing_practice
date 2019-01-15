95f701589869a5dde62d709f94b72fb264715e87
minyun168 <minyun168@outlook.com>
2019-01-15 23:51:03 +0800

    Update greeter2.py

--- pratice_of_python/greeter2.py
+++ pratice_of_python/greeter2.py
@@ -8,8 +8,15 @@ def formatted_name(first_name, last_name):
 # keep loop
 while True:
 	print("\nPlease tell me your name:")
+	print("(enter 'q' at any time to quit)")
+
 	f_name = input("first_name:")        # When we input we shoud code in str just like: 'A' ,'clear'; do not forget quotation marks
+	if f_name == 'q':
+		break
+
 	l_name = input("last_name:")
+	if l_name == 'q':
+		break
 	
 	formatted_name = formatted_name(f_name, l_name)
 	print("\n Hello, "+ formatted_namf + "!")
