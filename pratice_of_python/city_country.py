0ebb55945d0214d1060f22c4ef5d2da71a70d2f0
minyun168 <minyun168@outlook.com>
2019-01-16 21:47:48 +0800

    Create city_country.py

---
+++ pratice_of_python/city_country.py
@@ -0,0 +1,8 @@
+def city_country(city,country):
+	'''return a city with his country'''
+	city_country = city + " " + country
+	return city_country
+
+city_country = city_country('shanghai', 'china')
+print (city_country)
+	
