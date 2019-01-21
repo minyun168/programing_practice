cc077b169230335ae18198616d4a18a07d01bda3
minyun168 <minyun168@outlook.com>
2019-01-22 00:07:55 +0800

    Update city_country.py

--- pratice_of_python/city_country.py
+++ pratice_of_python/city_country.py
@@ -3,11 +3,11 @@ def city_country(city,country):
 	city_country = city + " " + country
 	return city_country
 
-city_country = city_country('shanghai', 'china')
-#print (city_country)
+city_country_1 = city_country('shanghai', 'china')
+print (city_country_1)
 
-city_country = city_country('tianjin', 'china')
-#
-city_country = city_country('tokyo', 'japanese')
-#
-	
+city_country_2 = city_country('tianjin', 'china')
+print (city_country_2)
+
+city_country_3 = city_country('tokyo', 'japanese')
+print (city_country_3)
