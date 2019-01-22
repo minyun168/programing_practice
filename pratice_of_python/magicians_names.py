e144c19911adcbb53c8f45f4d62684fe1ff941ba
minyun168 <minyun168@outlook.com>
2019-01-23 00:03:24 +0800

    Create magicians_names.py

---
+++ pratice_of_python/magicians_names.py
@@ -0,0 +1,10 @@
+#
+
+def magicians(magicians_names):
+	if magicians_names:
+		#magicians_name == magicians_names.pop
+		for magicians_name in magicians_names:
+			print (magicians_name)
+
+magicians_names = ['aclear', 'gido', 'konan']
+magicians(magicians_names)
