5f11c8bdfab1418fdbdc63a6022cfb249e2250cf
minyun168 <minyun168@outlook.com>
2019-01-26 20:56:02 +0800

    Update pizza.py

--- pratice_of_python/pizza.py
+++ pratice_of_python/pizza.py
@@ -1,10 +1,10 @@
 #
 
-def make_pizza(*toppings):
+def make_pizza(size,*toppings):
 	
-	print("\nMaking a pizza with the following tippings:")
+	print("\nMaking a " + str(size) + " inch pizza with the following tippings:")
 	for topping in toppings:
 		print("- " + topping)
 
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
+make_pizza(16,'pepperoni')
+make_pizza(12,'mushrooms', 'green peppers', 'extra cheese')
