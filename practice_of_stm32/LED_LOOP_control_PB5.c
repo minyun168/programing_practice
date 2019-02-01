298d849327ff3c584c3b8718cabf566e4ff5ebe7
minyun168 <minyun168@outlook.com>
2019-02-01 15:32:59 +0800

    Update LED_LOOP_control_PB5.c

--- practice_of_stm32/LED_LOOP_control_PB5.c
+++ practice_of_stm32/LED_LOOP_control_PB5.c
@@ -14,5 +14,25 @@ void LED_Init(void)
 	GPIO_Structure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_Structure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOB,&GPIO_Structure);
-	
+
+}
+
+int main(void)
+{
+	SystemInit();
+	LED_Init();
+
+	while (1)
+	{
+		GPIO_SetBits(GPIOB,GPIO_Pin_5);
+		Delay(0x8FFFF);
+		GPIO_ResetBits(GPIOB,GPIO_pin_5);
+		Delay(0x8FFFF);
+
+	}
+
+	void Delay(unsigned int nCount)
+	{
+		for(;nCount!=0;nCount--);
+	}
 }
