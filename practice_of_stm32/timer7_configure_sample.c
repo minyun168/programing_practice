90235f72758e28556daeef1d7350c98b05765055
minyun168 <minyun168@outlook.com>
2019-02-13 23:10:30 +0800

    Update timer7_configure_sample.c

--- practice_of_stm32/timer7_configure_sample.c
+++ practice_of_stm32/timer7_configure_sample.c
@@ -5,3 +5,14 @@ TIM_TimeBaseStructure.TIM_Prescaler = 36000-1;
 TIM_TimeBaseStructure.TIM_ClockDivision = i0;
 TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
 TIM_TimeBaseStructure.TIM_Period = 1000;
+
+
+// The configure sample of timer interrupt
+
+NVIC_InitTypeDef NVIC_InitStructure;
+NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+NVIC_InitStructure.NVIC_IRQChannel = TIM7_IRQn;
+NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+NVIC_Init(&NVIC_InitStructrue);
