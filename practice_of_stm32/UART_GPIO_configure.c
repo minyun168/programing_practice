4690c582a6f61622fd0e3e3910728190c1e414d6
minyun168 <minyun168@outlook.com>
2019-02-23 14:15:28 +0800

    Update UART_GPIO_configure.c

--- practice_of_stm32/UART_GPIO_configure.c
+++ practice_of_stm32/UART_GPIO_configure.c
@@ -1,7 +1,7 @@
 // This is a example of UART GPIO configuration 
 
 GPIO_InitTypeDef GPIO_InitStructure;
-GPIO_StructInit(&GPIO_InitStructure);
+GPIO_StructInit(&GPIO_InitStructure); //?
 GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9; //PA9 pin is for USART1_TX
 GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP; // Multiplexing push-pull output
 GPIO_Init(GPIOA,&GPIO_InitStructure);
