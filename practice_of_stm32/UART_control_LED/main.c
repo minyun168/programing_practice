c8eb5d304c7a44f8ef6d732cf1519f72b4407cdb
minyun168 <minyun168@outlook.com>
2019-02-25 18:09:49 +0800

    Update main.c

--- practice_of_stm32/UART_control_LED/main.c
+++ practice_of_stm32/UART_control_LED/main.c
@@ -20,10 +20,10 @@ int main(void)
 	memset(CmdBuffeer,10,0);
 	while(1)
 	{
-		if (strstr(CmdBuffer,"LEDON"))
+		if (strstr(CmdBuffer,"LEDON"))  // strstr: if "LEDON" in CmdBuffer, return address (as true). This function in "string.h"
 		{
 			GPIO_SetBits(GPIOB,GPIO_Pin_5);
-			memset(CmdBuffer,10,0);
+			memset(CmdBuffer,10,0);  //Set CmdBuffer to 0
 		}
 		if (strstr(CmdBuffer,"LEDOFF"))
 		{
@@ -33,3 +33,66 @@ int main(void)
 	}
 
 }
+
+
+void GPIO_Configuration(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB,&GPIO_InitStructure);
+
+	GPIO_StructInit(&GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+} 
+
+
+void RCC_Configuration(void)
+{
+	SystemInit();
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
+}
+
+
+void USART_Config(void)
+{
+	USART_InitTypeDef USART_InitStructure;
+	USART_InitStructure.USART_BaudRate = 115200;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx; //Set USART work in receive mode
+	USART_Init(USART1,&USART_InitStructure);
+	USART_ITConfig(USART1,USART_IT_RXNE,ENABLE); //Open USART1 to receiving the interrupt
+	USART_Cmd(USART1,ENABLE);
+
+}
+
+
+void NVIC_Configuration(void)
+{
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
+
+	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+}
+
+
+ 
