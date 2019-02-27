9a7cee182a78ad101665b99e4a77d0f63a038bd1
minyun168 <minyun168@outlook.com>
2019-02-27 12:51:47 +0800

    Update main.c

--- practice_of_stm32/DMA_peripheral_to_memory/main.c
+++ practice_of_stm32/DMA_peripheral_to_memory/main.c
@@ -1,3 +1,5 @@
+//Pc use "serial debugging assistant" send "LED" to development board; when development board receive the data, transfer it to buffer, the main loop will scan whether receive the data, if have receive, send the data to serial port
+
 #include "stm32f10x.h"
 #include "stm32f10x_usart.h"
 #include "stm32f10x_dma.h"
@@ -11,7 +13,7 @@ int fputc(int ch, FILE *f);
 
 uint8_t SrcBuf[12]="xxxxxxxxxxx";
 {
-	uint8_t flag=1;
+	uint8_t flag=1; //flag is for loop
 	RCC_Configuration();
 	GPIO_Configuration();
 	USART_Config();
@@ -32,3 +34,46 @@ uint8_t SrcBuf[12]="xxxxxxxxxxx";
 }
 
 
+void GPIO_Configuration(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_INitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+
+}
+
+
+void RCC_Configuration(void)
+{
+	SystemInit();
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1,ENABLE); // DMA1 shoud use AHB clock
+
+}
+
+
+void USART_Config(void)
+{
+	USART_InitTypeDef USART_InitStructure;
+	UsART_InitStructure.USART_BaudRate = 115200;
+	USART_InitStructure.USART_WordLength = UsSART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = UsART_StopBits_1;
+	UsARt_InitStructure.USART_Parity = USSART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowcontrol = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Tx;
+	USART_Init(USART1,&USART_InitStructure);
+	USART_Cmd(USART1,ENABLE);
+
+}
+
+
+void DMA_Configuration(void)
+{
+	
+}
