82e052eeab3e1b19988a1c40d3cb7e81b73ec8e4
minyun168 <minyun168@outlook.com>
2019-03-04 18:46:34 +0800

    Update main.c

--- practice_of_stm32/ADC_base_sample/main.c
+++ practice_of_stm32/ADC_base_sample/main.c
@@ -121,5 +121,31 @@ void DMA_Configuation(void)
 	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&ADC1->DR;
 	DMA_InitStructure.DMA_MemoryBaseAddr = (u32)&ADC_ConvertedValue;
 	DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
+	DMA_InitStructure.DMA_BufferSize = 1;
+	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Disable;
+	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+	DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+	DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+	DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+
+	DMA_Init(DMA1_Channel1,&DMA_InitStructure);
+	DMA_Cmd(DMA1_Channel1,ENABLE);
+
+}
+
+
+int fputc(int ch,FILE *f)
+{
+	if (ch == '\n')
+	{
+		while(USART_GetFlagStatus(USART1,USART_FLAG_TC)==RESET);
+		USART_SendData(USART1,'\r');
+
+	}
+	while(USART_GetFlagStatus(USART1,USART_FLAG_TC)==RESET);
+	USART_SendData(USART1,ch);
+	return ch;
 	
 }
