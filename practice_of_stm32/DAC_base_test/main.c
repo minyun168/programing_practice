2c41956a6405e6f57b801e8a4dc804f5b1bfbc0a
minyun168 <minyun168@outlook.com>
2019-03-07 23:46:22 +0800

    Update main.c

--- practice_of_stm32/DAC_base_test/main.c
+++ practice_of_stm32/DAC_base_test/main.c
@@ -58,6 +58,15 @@ void RCC_Configuration(void)
 
 void USART_Config(void)
 {
+	USART_InitTypeDef USART_InitStructure;
+	USART_InitStructure.USART_BaudRate = 115200;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_USART_Mode = USART_Mode_Tx;
+	USART_Init(USART1,&USART_InitStructure);
+	USART_Cmd(USART1,ENABLE);
 	
 }
 
