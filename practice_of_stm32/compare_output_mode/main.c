56f71af47408d0d7bbc6f7aae8de1c85d9b854b6
minyun168 <minyun168@outlook.com>
2019-03-11 00:32:30 +0800

    Update main.c

--- practice_of_stm32/compare_output_mode/main.c
+++ practice_of_stm32/compare_output_mode/main.c
@@ -1,4 +1,4 @@
-//
+// Make a output wave profile
 #include "stm32f10x.h"
 #include "stm32f10x_tim.h"
 #include "stm32f10x_rcc.h"
@@ -9,7 +9,7 @@ void GPIO_Configuration(void);
 void NVIC_Configuration(void);
 void TIM_Configuration(void);
 
-int main()
+int main(void)
 {
 	RCC_Configuration();
 	GPIO_Configuration();
@@ -20,4 +20,58 @@ int main()
 	{
 	
 	}
+}
+
+
+void RCC_Configuration(void)
+{
+	SystemInit();
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3,ENABLE);	
+
+}
+
+
+void GPIO_Configuration(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIP_InitStructure.GPIO_Pin = GPIO_Pin_5;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB,&GPIO_InitStructure);
+
+}
+
+
+void NVIC_Configuration(void)
+{
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+	NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
+	NVIC_IntiStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+}
+
+
+void TIM_Configuration(void)
+{
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+	TIM_OCInitTypeDef TIM_OCInitStructure; //Because this is general purpose timer, so define another structure
+
+	TIM_TimeBaseStructure.TIM_Prescaler = 36000-1; //Configure the frequency as 2KHz
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseStructure.TIM_Period = 65535; //here differnt with base_timer_test 
+	TIM_TimeBaseInit(TIM3,&TIM_TimeBaseStructure);
+
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_Timing; // Work in compare_output_mode
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCInitStructure.TIM_Pulse = 200; //
+	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High; //set output polarity is high
+	TIM_OC1Init(TIM3,&TIM_InitStructure);
+	TIM_Cmd(TIM3,ENABLE);
+	TIM_ITConfig(TIM3,TIM_IT_CC1,ENABLE);
+
 }
