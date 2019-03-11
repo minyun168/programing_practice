ba03d326924e6a07bf960a847cd597facf49732f
minyun168 <minyun168@outlook.com>
2019-03-12 00:24:49 +0800

    Update stm32f10x_it.c

--- practice_of_stm32/basic_timer_test/stm32f10x_it.c
+++ practice_of_stm32/basic_timer_test/stm32f10x_it.c
@@ -1,8 +1,8 @@
 void TIM7_IRQHandler(void)
 {
-	if(TIM_GetITStatus(TIM7,TIM_IT_Update)==SET)
+	if(TIM_GetITStatus(TIM7,TIM_IT_Update)==SET) //if have interrupt
 	{
-		GPIO_WriteBit(GPIOB,GPIO_Pin_5,(BitAction)(1-GPIO_ReadOutputDataBit(GPIOB,GPIO_Pin_5)));
-		TIM_ClearITPendingBit(TIM7,TIM_FLAG_Update);
+		GPIO_WriteBit(GPIOB,GPIO_Pin_5,(BitAction)(1-GPIO_ReadOutputDataBit(GPIOB,GPIO_Pin_5))); //if pin PB_5 is 1, make it be 0; if pin PB_5 is 0, make it be 1. 
+		TIM_ClearITPendingBit(TIM7,TIM_FLAG_Update); //function "TIM_ClearITPendingBit": clear interrupt flag "TIM_FLAG_Update"
 	}
 }
