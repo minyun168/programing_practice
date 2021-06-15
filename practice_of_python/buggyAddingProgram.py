fde021e8bfad7225feaca49899bf865ab2ff862c
minyun168 <minyun168@outlook.com>
2021-06-15 15:32:07 +0900

    Create buggyAddingProgram.py

---
+++ practice_of_python/buggyAddingProgram.py
@@ -0,0 +1,10 @@
+#! python 2.7
+# sum of numbers
+
+print ('Enter the first  number to add:')
+first=input()
+print ('Enter the second number to add.')
+second=input()
+print ('Enter the third number to add.')
+third=input()
+print('The sum is %s' %(first+second+third))
